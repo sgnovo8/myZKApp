fe1efe5bca7f8876c5e4d700d54fd89d4344777b
sgnovo8 <sgnovo8@gmail.com>
2023-10-10 22:12:39 -0400

    test commit

--- src/Add.ts
+++ src/Add.ts
@@ -9,17 +9,4 @@ import { Field, SmartContract, state, State, method } from 'o1js';
  *
  * This file is safe to delete and replace with your own contract.
  */
-export class Add extends SmartContract {
-  @state(Field) num = State<Field>();
-
-  init() {
-    super.init();
-    this.num.set(Field(1));
-  }
-
-  @method update() {
-    const currentState = this.num.getAndAssertEquals();
-    const newState = currentState.add(2);
-    this.num.set(newState);
-  }
-}
+export class AgeVerifier extends SmartContract {}
